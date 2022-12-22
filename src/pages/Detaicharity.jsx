249c13724063de55a384385f759050d16f58e917
UsmanYap <107064481+UsmanYap@users.noreply.github.com>
2022-12-22 08:03:45 +0700

    menambaghkan detail charity

---
+++ src/pages/Detaicharity.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MyCharityCards from "../components/Cards/MyCharityCards";
+import { Link } from "react-router-dom";
+
+const DetailPage = () => {
+  return <detailcharity />;
+};
+
+export default Detailcharity;
